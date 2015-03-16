efe003893af34e4929fb9a3616742fe944fb1615
Konrud <konrud5@gmail.com>
2015-03-16 20:48:00 +0200

    Android-List-Shadow-Effect Beta 1
    First commit of the Android-List-Shadow-Effect . Has issue with
    setTimeout which triggers several times.

---
+++ Scripts/jquery.android-list-shadow.js
@@ -0,0 +1,39 @@
+﻿;(function ($) {
+
+    $.fn["listShadow"] = function (prop) {
+
+        prop = $.extend({}, $.fn.listShadow.defaults, prop);
+
+        return this.each(function (index, elem) {
+
+            $(elem).on("scroll mousewheel", function (eventObj) {
+                var $self = $(this);
+                var currentPosition = $self.scrollTop();
+                var top = 0;
+                var bottom = this.scrollHeight - this.clientHeight;
+
+                if (currentPosition == top || currentPosition >= bottom) {
+                    $self.addClass(prop.className, prop.addSpeed, prop.addEasing, function () {
+                      var timeOut =  setTimeout(function () {
+                          $self.removeClass(prop.className, prop.removeSpeed, prop.removeEasing);
+                          clearTimeout(timeOut);
+                        }, prop.removeTimeOut);
+                    });
+                }
+
+            });
+
+        });
+    };
+
+    /// Default properties
+    $.fn.listShadow.defaults = {
+        addSpeed: 400,
+        addEasing: "linear",
+        removeSpeed: 600,
+        removeEasing: "linear",
+        removeTimeOut: 700,
+        className: "android-list-shadow"
+    };
+
+})(jQuery);
